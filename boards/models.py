1bf1650cc64803f6530e61bf2c24d71ab0b26acf
landryPrct <landryhabonimana@outlook.com>
2022-03-12 10:45:05 +0200

    Dans Boards.models ajout d' un def __str__ pour retourner les string ; dans views affichages des resultats de la base de donnees

--- boards/models.py
+++ boards/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+# Create your models here.d
 class Board(models.Model):
+
     name = models.CharField(max_length=40, unique=True)
     description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Topic(models.Model):
