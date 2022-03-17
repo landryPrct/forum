295b4ea6f2247eb93dfa728f62b75c5104df8814
landryPrct <landryhabonimana@outlook.com>
2022-03-17 21:08:57 +0200

    afficher celui qui est le dernier a publie et le nombre de publications

--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewTopicForm
-from .models import Board, Topic, Post
+from .forms import NewTopicForm, PostForm
+from .models import Board, Post, Topic
 
 
 def home(request):
@@ -15,6 +14,7 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+
 @login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
@@ -23,14 +23,14 @@ def new_topic(request, pk):
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = request.user  # <- here
+            topic.starter = request.user
             topic.save()
             Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=request.user  # <- and here
+                created_by=request.user
             )
-            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+            return redirect('topic_posts', pk=pk, topic_pk=topic.pk)
     else:
         form = NewTopicForm()
     return render(request, 'new_topic.html', {'board': board, 'form': form})
@@ -39,3 +39,19 @@ def new_topic(request, pk):
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
     return render(request, 'topic_posts.html', {'topic': topic})
+
+
+@login_required
+def reply_topic(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.topic = topic
+            post.created_by = request.user
+            post.save()
+            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+    else:
+        form = PostForm()
+    return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
--- forum/urls.py
+++ forum/urls.py
@@ -57,7 +57,7 @@ urlpatterns = [
     path('boards/<str:pk>/', views.board_topics, name='board_topics'),
     path('boards/<str:pk>/new/', views.new_topic, name='new_topic'),
     path('boards/<str:pk>/topics/<str:topic_pk>/', views.topic_posts, name='topic_posts'),
-
+    path('boards/<str:pk>/topics/<str:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('admin/', admin.site.urls),
     ]
 
