616198a49ef7cecdae16412e77c7a02b112f0d34
landryPrct <landryhabonimana@outlook.com>
2022-03-18 16:07:40 +0200

    pagination reutilisable

--- forum/urls.py
+++ forum/urls.py
@@ -56,7 +56,7 @@ urlpatterns = [
     # path('boards/', views.board_topics, name='board_topics'),
     path('boards/<str:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('boards/<str:pk>/new/', views.new_topic, name='new_topic'),
-    path('boards/<str:pk>/topics/<str:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('boards/<str:pk>/topics/<str:topic_pk>/', views.PostListView.as_view(), name='topic_posts'),
     path('boards/<str:pk>/topics/<str:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
     path('boards/(<str:pk>/topics/<str:topic_pk>/posts/<str:post_pk>/edit/',views.PostUpdateView.as_view(), name='edit_post'),
 
